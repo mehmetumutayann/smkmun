89369f9e0c48d66cbcaeeb7f0a0812be3231dd3b
Mehmet Umut Ayan <mehmetumutayan2007@gmail.com>
2023-06-25 23:13:58 +0300

    upd

---
+++ comite.js
@@ -0,0 +1,8 @@
+const hoverlimg = document.querySelector(".resim")
+
+
+function altagelsin(hoverlimg) {
+    hoverlimg.style.position = "absolute";
+    hoverlimg.style.top = "100%";
+    hoverlimg.style.transform = "translateY(-100px)";
+}
