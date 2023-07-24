1d303cf3b9c0a4495400bef9498acf0f99c0f292
Mehmet Umut Ayan <mehmetumutayan2007@gmail.com>
2023-07-24 22:08:32 +0300

    update

--- script.js
+++ script.js
@@ -26,10 +26,10 @@
           const now = new Date().getTime(),
                 distance = countDown - now;
   
-          document.getElementById("days").innerText = Math.floor(distance / (day)),
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+          document.getElementById("days").innerText = "00",
+            document.getElementById("hours").innerText ="00",
+            document.getElementById("minutes").innerText = "00",
+            document.getElementById("seconds").innerText = "00";
   
           //do something later when date is reached
           if (distance < 0) {
